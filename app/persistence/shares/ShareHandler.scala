04a5daf2114dfadde5b1cc2fdf6c35d1e00bc2d4
K-Singh <kiratgsingh@gmail.com>
2022-06-02 09:44:13 -0400

    Fixed GroupExec separation

--- app/persistence/shares/ShareHandler.scala
+++ app/persistence/shares/ShareHandler.scala
@@ -19,13 +19,17 @@ class ShareHandler(paymentType: PaymentType, blockMiner: String, db: PostgresPro
   final val SHARE_LIMIT = 50000
   logger.info(s"ShareHandler is using payment type ${paymentType.toString}")
   def queryToWindow(block: SPoolBlock, defaultTag: String): ShareCollector = {
-    logger.info(s"Share handler querying to window for block ${block.blockheight}")
+    logger.info(s"Share handler querying to window for block ${block.blockheight} with creation date ${block.created}")
     var offset = 0
     val miners = Await.result(db.run(Tables.PoolSharesTable.queryPoolMiners(block.poolTag, defaultTag)), 60 seconds).map(m => m.address -> m.subpool).toMap
     while(collector.totalScore < AppParameters.pplnsWindow && offset != -1){
       val fShares = db.run(Tables.PoolSharesTable.queryBeforeDate( block.created, offset, SHARE_LIMIT))
       val shares = Await.result(fShares, 400 seconds).filter(sh => miners.contains(sh.miner))
       logger.info(s"${shares.size} shares were queried")
+      logger.info(s"Share batch start: ${shares.head.created}")
+      logger.info(s"Share batch end: ${shares.last.created}")
+      logger.info(s"Share contains: ${shares.count(_.miner == "9faXjKXBSgQVwnH3TMpRaRC5mv39kr3SLdKZzEkBk9CRCkCEzjx")}")
+      logger.info(s"Current offset: ${offset}")
       shares.foreach{
         s =>
           val ps = PartialShare(s.miner, s.difficulty, s.networkdifficulty, miners(s.miner))
