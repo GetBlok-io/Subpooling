79451e0ecd49ce29f33f3f057586354ba8d74457
K-Singh <kiratgsingh@gmail.com>
2022-06-05 20:43:32 -0400

    Added logging to prop emissions root

--- app/persistence/shares/ShareHandler.scala
+++ app/persistence/shares/ShareHandler.scala
@@ -53,7 +53,7 @@ class ShareHandler(paymentType: PaymentType, blockMiner: String, db: PostgresPro
       offset = offset + 50000
       if(shares.isEmpty)
         offset = -1
-      if(offset >= 500000)
+      if(offset >= 100000)
         offset = -1
     }
     collector
