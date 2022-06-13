bd3b64c7d6275c9aa7e4e5ccec1b9d6ec598c73a
K-Singh <kiratgsingh@gmail.com>
2022-06-13 18:15:28 -0400

    Added effort to blok bot

--- app/actors/PushMessageNotifier.scala
+++ app/actors/PushMessageNotifier.scala
@@ -42,7 +42,8 @@ class PushMessageNotifier @Inject()(wsClient: WSClient, config: Configuration) e
           if(params.enableBlockBot) {
             logger.debug(s"Sending Push Notification for block ${poolBlock.blockheight}")
             val msgString = s"Block ${poolBlock.blockheight} was found by miner:\n${poolBlock.miner}\nin pool:\n#[${Helpers.trunc(poolBlock.poolTag)}]\n(${poolInfo.title})" +
-              s"\nThe pool has now found ${poolBlock.gEpoch} blocks."
+              s"\nThe pool has now found ${poolBlock.gEpoch} blocks." +
+              s"\nThe block's effort was ${(poolBlock.effort.get * 100).toString}%"
             val request: WSRequest = wsClient.url(blockBotAPI)
             val resp = request
               .withQueryStringParameters("chat_id" -> s"$blockBotChat", "text" -> msgString)
