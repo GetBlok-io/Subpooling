2366144b5f3f311d975f945053e35cf618d435c3
K-Singh <kiratgsingh@gmail.com>
2022-06-13 10:51:41 -0400

    Changed token change fix amount

--- subpooling_core/src/main/scala/contracts/holding/TokenHoldingContract.scala
+++ subpooling_core/src/main/scala/contracts/holding/TokenHoldingContract.scala
@@ -45,7 +45,7 @@ class TokenHoldingContract(holdingContract: ErgoContract) extends HoldingContrac
     val totalOwedPayouts =
       lastDistribution.filter(c => c._2.getStored < c._2.getMinPay).dist.map(c => c._2.getStored).sum
     // Removed 7 tokens to help with change, should find better solution later
-    val totalRewards = holdingBoxTokens - totalOwedPayouts - 10
+    val totalRewards = holdingBoxTokens - totalOwedPayouts - 11
     val feeList = currentPoolFees.fees.map{
       // Pool fee is defined as x/100000 of total inputs value.
       poolFee =>
