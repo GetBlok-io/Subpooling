bb800541a6edd45f52930dff76ed5de4d7807fe2
K-Singh <kiratgsingh@gmail.com>
2022-06-18 23:46:00 -0400

    Created regen from states

--- app/tasks/DbCrossCheck.scala
+++ app/tasks/DbCrossCheck.scala
@@ -226,6 +226,12 @@ class DbCrossCheck @Inject()(system: ActorSystem, config: Configuration,
     val outputBoxes = (expReq ? BoxesByTokenId(ErgoId.create("30afb371a30d30f3d1180fbaf51440b9fa259b5d3b65fe2ddc988ab1e2a408e7"), 0, 500))
       .mapTo[Option[Seq[Output]]]
     logger.info("Now regening states")
+    outputBoxes.onComplete{
+      case Failure(exception) =>
+        logger.error("There was an exceptiong getting outboxes", exception)
+      case Success(value) =>
+        logger.info("Values found!")
+    }
     outputBoxes.map{
       optOutputs =>
         logger.info(s"${optOutputs.get.length} outputs found!")
