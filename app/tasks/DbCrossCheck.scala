86b7bf52e20212ff63aff83d0116354c13623c81
K-Singh <kiratgsingh@gmail.com>
2022-06-18 23:46:12 -0400

    Created regen from states

--- app/tasks/DbCrossCheck.scala
+++ app/tasks/DbCrossCheck.scala
@@ -223,7 +223,7 @@ class DbCrossCheck @Inject()(system: ActorSystem, config: Configuration,
   def regenStates = {
     implicit val timeout: Timeout = Timeout(1000 seconds)
     logger.info("Regening states")
-    val outputBoxes = (expReq ? BoxesByTokenId(ErgoId.create("30afb371a30d30f3d1180fbaf51440b9fa259b5d3b65fe2ddc988ab1e2a408e7"), 0, 500))
+    val outputBoxes = (expReq ? BoxesByTokenId(ErgoId.create("30afb371a30d30f3d1180fbaf51440b9fa259b5d3b65fe2ddc988ab1e2a408e7"), 0, 100))
       .mapTo[Option[Seq[Output]]]
     logger.info("Now regening states")
     outputBoxes.onComplete{
