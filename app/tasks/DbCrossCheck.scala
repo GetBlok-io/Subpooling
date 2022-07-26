5c32034cdfb811f94fec49fa6349868039ba2391
K-Singh <kiratgsingh@gmail.com>
2022-07-26 14:33:26 -0400

    Added make history

--- app/tasks/DbCrossCheck.scala
+++ app/tasks/DbCrossCheck.scala
@@ -104,12 +104,12 @@ class DbCrossCheck @Inject()(system: ActorSystem, config: Configuration,
 //              logger.error("There was a critical error while re-generating dbs!", ex)
 //              Failure(ex)
 //          }
-          logger.info("Now making backup state")
-          new File(AppParameters.plasmaStoragePath + s"/backup").mkdir()
-          val backupState = new BalanceState("backup")
-
-          logger.info(s"Current digest for backup state: ${backupState.map.toString()}")
-          syncState(backupState)
+          db.run(Tables.PoolBlocksTable
+            .filter(_.poolTag === "f0f3581ea3aacf37c819f0f18a47585866aaf4c273d0c3c189d79b7d5fc71e80")
+            .filter(_.gEpoch > 23L)
+            .map(_.status)
+            .update(PoolBlock.CONFIRMED)
+          )
         }
     })(contexts.taskContext)
   }
