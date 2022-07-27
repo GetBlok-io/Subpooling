fe6b94f091e1ebba21a52116b4fd90a7bf99744e
K-Singh <kiratgsingh@gmail.com>
2022-07-26 22:48:54 -0400

    Updated to Plasma 0.0.8

--- app/tasks/DbCrossCheck.scala
+++ app/tasks/DbCrossCheck.scala
@@ -104,12 +104,8 @@ class DbCrossCheck @Inject()(system: ActorSystem, config: Configuration,
 //              logger.error("There was a critical error while re-generating dbs!", ex)
 //              Failure(ex)
 //          }
-          //initBackup()
-          db.run(Tables.PoolBlocksTable
-          .filter(_.poolTag === "f0f3581ea3aacf37c819f0f18a47585866aaf4c273d0c3c189d79b7d5fc71e80")
-          .filter(_.gEpoch > 25L)
-          .map(_.status)
-          .update(PoolBlock.PROCESSED))
+          initBackup()
+
         }
     })(contexts.taskContext)
   }
