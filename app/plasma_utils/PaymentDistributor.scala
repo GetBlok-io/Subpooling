7bdde1d1dd097553d4606ea3891fbe3a2358328e
K-Singh <kiratgsingh@gmail.com>
2022-10-12 23:22:27 -0400

    Added count limit to share queries

--- app/plasma_utils/PaymentDistributor.scala
+++ app/plasma_utils/PaymentDistributor.scala
@@ -265,7 +265,7 @@ class PaymentDistributor(expReq: ActorRef, stateHandler: ActorRef,
   // TODO: Currently vertically scaled, consider horizontal scaling with Seq[BatchSelections]
   def collectInputs(batchSelection: BatchSelection): Seq[InputBox] = {
     if(batchSelection.info.currency == PoolInformation.CURR_ERG) {
-      val blockSum = Helpers.ergToNanoErg(batchSelection.blocks.map(_.reward).sum) + (Helpers.OneErg * 2)
+      val blockSum = Helpers.ergToNanoErg(batchSelection.blocks.map(_.reward).sum) + (Helpers.OneErg * 1.3).toLong
       boxLoader.collectFromLoaded(blockSum).toSeq
     }else{
       val blockSum = Helpers.OneErg * 3
